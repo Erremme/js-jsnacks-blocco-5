3857a6c9036d2979701605005e9e428c78c29f39
Erremme <Erremme2.0@gmail.com>
2024-12-24 15:38:33 +0100

    feat : make snack6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,20 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+
+const zucchineGrandi = [];
+const altreZucchine = [];
+
+ zucchine.forEach( (elm) =>{
+  if(elm.length > 15){
+    zucchineGrandi.push(elm)
+  }else{
+    altreZucchine.push(elm)
+  }
+}
+
+
+)
+console.log(zucchineGrandi)
+console.log(altreZucchine)
